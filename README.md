a5281e8d86863c2405732c8daca601126d913d58
1Blademaster <blademaster1101@gmail.com>
2022-04-02 23:15:25 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ucard-replicator
 A small device meant to replicate CODE-39 barcodes on an e-ink display using a Raspberry Pi Pico
+
+- - - 
+*Using Pico-Go on VSCode 1.65.2*
+
+*My E-Ink display*: https://www.waveshare.com/wiki/Pico-ePaper-2.66
+
+### To run:
+
+First transfer `epd.py` onto the Pico's filesystem, then you can run `main.py`.
+
+Currently when `main.py` is ran on the Pico, you can input a UCard number and it will be shown as a code-39 barcode.
+
+> I have not extensively tested the barcode printing functionality. It may not work on different screen sizes and edge cases have not been tested.
+
+- - -
+
+##### ToDo:
+- Add a button to switch through a predefined set of barcodes
+- Add another button to create custom barcodes
+- Tidy up code
+- Add a switch and connect the Pico to a small LiPo battery
---
+++ main.py
@@ -0,0 +1,164 @@
+from epd import EPD_2in66
+import utime
+
+Y_START = 25
+Y_END = THIN_BAR00
+THIN_BAR = 1
+WIDE_BAR = 3
+
+def draw(char, x, nextN=None):
+    if char == '*':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00) # thin black
+        # wide white
+        # gap 4
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00) #thin black
+        # thin white
+        # gap 4
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00) # wide black
+        # thin white
+        # gap 4
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00) # wide black
+        # # thin white
+        # # gap 2 ?
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00) # thin black
+    elif char == '0':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 6
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        if nextN in ['1', '3', '5', '8']:
+            x -= 2
+    elif char == '1':
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+    elif char == '2':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+    elif char == '3':
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+    elif char == '4':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 6
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+    elif char == '5':
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 6
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+    elif char == '6':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 6
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+    elif char == '7':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+    elif char == '8':
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+    elif char == '9':
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+        x -= 4
+        epd.fill_rect(Y_START, x, Y_END, WIDE_BAR, 0x00)
+        x -= 2
+        epd.fill_rect(Y_START, x, Y_END, THIN_BAR, 0x00)
+
+    return x - 2
+
+'''
+
+(y, x)
+0, 296  ------ 0, 0
+                USB ->
+122, 296 --- 152, 0
+
+0xff = white
+0x00 = black
+
+fill_rect(y, x, h, w)
+
+'''
+
+if __name__ == '__main__':
+    ucard_number = '*' + input('Enter a UCard number: ') + '*'
+
+    epd = EPD_2in66()
+    epd.Clear(0xff)
+    epd.fill(0xff)
+
+    x = 236
+
+    for idx, n in enumerate(ucard_number):
+        try:
+            x = draw(n, x, ucard_number[idx+1])
+        except IndexError:
+            x = draw(n, x)
+
+    epd.text(ucard_number.replace('*', ''), 38, 15, 0x00)
+            
+    epd.display(epd.buffer)
