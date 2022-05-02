4a34aca9a79544ffca2b772a342dfbeb941885d6
1Blademaster <blademaster1101@gmail.com>
2022-05-02 19:45:26 +0100

    Added barcode changing via button

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from epd import EPD_2in66
+import time
 import utime
+
+from machine import Pin
+
+from epd import EPD_2in66
+
 '''
 
 (y, x)
@@ -23,6 +28,25 @@ Y_END = 100
 THIN_BAR = 1
 WIDE_BAR = 3
 
+ASTERIX = 'nsnwwn'
+
+NUMBERS = [
+    'nnswwn', 'wnsnnw', 'nwsnnw', 'wwsnnn', 'nnswnw', 'wnswnn', 'nwswnn',
+    'nnsnww', 'wnsnwn', 'nwsnwn'
+]
+
+UCARDS = [
+    '001760667',
+    '001760668',
+    '001760669',
+    '001713214',
+]
+
+btn = Pin(14, Pin.IN, Pin.PULL_UP)
+
+btn_last_pressed = time.ticks_ms()
+
+current_index = 0
 
 def drawBarcode(letter, x):
     for idx, b in enumerate(letter):
@@ -47,32 +71,39 @@ def drawBarcode(letter, x):
             else:
                 x -= 4
 
-
-if __name__ == '__main__':
-    ucard_number = input('Enter a UCard number: ')
-
-    epd = EPD_2in66()
+def displayUcard(ucard_number):
     epd.Clear(0xff)
     epd.fill(0xff)
 
     x = 246
 
-    asterix = 'nsnwwn'
-
-    numbers = [
-        'nnswwn', 'wnsnnw', 'nwsnnw', 'wwsnnn', 'nnswnw', 'wnswnn', 'nwswnn',
-        'nnsnww', 'wnsnwn', 'nwsnwn'
-    ]
-
-    code = asterix
+    code = ASTERIX
 
     for n in ucard_number:
-        code += numbers[int(n)]
+        code += NUMBERS[int(n)]
 
-    code += asterix
+    code += ASTERIX
 
     drawBarcode(code, x)
 
     epd.text(ucard_number, 38, 15, 0x00)
 
     epd.display(epd.buffer)
+
+def btnHandler(pin):
+    global btn_last_pressed, UCARDS, current_index
+    if time.ticks_diff(time.ticks_ms(), btn_last_pressed) > 1000:
+        displayUcard(UCARDS[current_index % len(UCARDS)])
+
+        current_index += 1
+
+        btn_last_pressed = time.ticks_ms()
+
+if __name__ == '__main__':
+    # ucard_number = input('Enter a UCard number: ')
+
+    epd = EPD_2in66()
+
+    btn.irq(trigger=Pin.IRQ_RISING, handler=btnHandler)
+
+    # displayUcard(ucard_number)
